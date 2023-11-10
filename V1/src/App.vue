809499e4853fe03eb9c3f61cf4bb736675aa4882
TristanBourbigot <105348197+TristanBourbigot@users.noreply.github.com>
2023-11-10 13:22:27 +0100

    add vuetify

--- V1/src/App.vue
+++ V1/src/App.vue
@@ -9,20 +9,25 @@ export default {
           nbTurn : 0,
           nbPlayeurToPlay : 0,
           nbPlayeur : 2,
-          playeurName : ["Joueur 1", "Joueur 2"],
-          card : [[],[],[],[]]
+          playeurName : ["Joueur 1", "Joueur 2", "Joueur 3", "Joueur 4"],
+          card : [[],[],[],[]],
+          dialog : true,
+          rules: {
+            required: value => !!value || 'Champs requis',
+            maxNbPlayeur: value => value <= 4 || 'Max 4 joueurs',
+            minNbPlayeur: value => value >= 2 || 'Min 2 joueurs',
+          },
         }
   },
   methods :{
-    createGame(nbPlayer, playeurName){
-      this.nbPlayeur = nbPlayer;
-      this.playeurName = playeurName;
+    createGame(){
+      this.dialog = false;
       this.nbPlayeurToPlay = 1;
-      this.playeurToPlay = this.playeurName[this.nbPlayeurToPlay];
+      this.playeurToPlay = this.playeurName[this.nbPlayeurToPlay-1];
       this.nbTurn = 0;
     },
     playCard(idCol){
-      document.getElementById(idCol).innerHTML = "<img src='/src/assets/card/"+this.cardToPlay+"' id='cardToPlay' height='123px' width='123px'> ";
+      document.getElementById(idCol).innerHTML = "<img src='/src/assets/card/"+this.cardToPlay+"' id='cardToPlay' height='57px' width='57px'> ";
       changePlayeur();
     },
     changePlayeur(){
@@ -39,10 +44,11 @@ export default {
 </script>
 
 <template>
+
 <div class="plateau">
   <div class="grille">  
-    <div :class="'row-'+i"  v-for="i in 6" :key="i">
-        <div :class="'col col-'+i+j"  :id="'col-'+i+j" v-for="j in 6" :key="j" v-on:click="playCard('col-'+i+j)">
+    <div :class="'row-'+i"  v-for="i in 11" :key="i">
+        <div :class="'col col-'+i+j"  :id="'col-'+i+j" v-for="j in 11" :key="j" v-on:click="playCard('col-'+i+j)">
         </div>
     </div>
   </div>
@@ -60,6 +66,71 @@ export default {
   </div>
 </footer>
 
+  <template>
+    <v-dialog
+      v-model="dialog"
+      transition="dialog-bottom-transition"
+      width="500px"
+    >
+      <v-card>
+        <v-toolbar
+          color="#7b7b7b"
+          title="Créer une partie"
+        ></v-toolbar>
+        <v-card-text style="background-color:#cfcfcf;">
+          <v-container>
+            <v-row class="justify-center">
+              <v-col cols="1" sm="10" md="10">
+                <v-text-field
+                  :rules="[rules.required , rules.maxNbPlayeur, rules.minNbPlayeur]"
+                  v-model="nbPlayeur"
+                  label="Nb joueurs"
+                  type="number"
+                  width="50px"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="4" sm="6" md="6">
+                <v-text-field
+                  :rules="[rules.required]"
+                  v-model="playeurName[0]"
+                  label="Nom du joueur 1"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="4" sm="6" md="6">
+                <v-text-field
+                  :rules="[rules.required]"
+                  v-model="playeurName[1]"
+                  label="Nom du joueur 1"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col v-if="nbPlayeur>=3" cols="4" sm="6" md="6">
+                <v-text-field
+                  :rules="[rules.required]"
+                  v-model="playeurName[2]"
+                  label="Nom du joueur 1"
+                ></v-text-field>
+              </v-col>
+              <v-col v-if="nbPlayeur>=4" cols="4" sm="6" md="6">
+                <v-text-field
+                  :rules="[rules.required]"
+                  v-model="playeurName[3]"
+                  label="Nom du joueur 1"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions style="background-color:#cfcfcf;">
+          <v-btn color="primary" block @click="createGame()">Créer la partie</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+  </template>
 </template>
 
 <style scoped>
@@ -95,22 +166,22 @@ Custom Grid
   padding-top: 40px;
   text-align: center;
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-template-rows: repeat(6, 1fr);
-  height: 825px;
-  width: 750px;
+  grid-template-columns: repeat(11, 1fr);
+  grid-template-rows: repeat(11, 1fr);
+  height: 700px;
+  width: 660px;
 }
 
 .col{
   border: solid #cfcfcf 1.5px;
-  height: 125px;
-  width: 125px;
+  height: 60px;
+  width: 60px;
+  padding: 0px;
+  margin: 0px;
 }
 
 .col:hover{
   border: solid #cfcfcf 1.5px;
-  height: 125px;
-  width: 125px;
   background: #7b7b7b;
 }
 
